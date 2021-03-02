cebf2d6377646347c94e79ca6b1f3452a87750b1
Karl Gutwin <karl@bioteam.net>
2021-03-02 14:34:58 -0500

    Refactoring common reference

--- octoprint_display_panel/__init__.py
+++ octoprint_display_panel/__init__.py
@@ -20,6 +20,41 @@ import inspect
 import adafruit_ssd1306
 import RPi.GPIO as GPIO
 
+## REFACTORING SCOPES
+##   Changes to the following are scoped to a specific branch, and should
+##   not be modified by another branch. Conflicting changes should be done
+##   in the top-level refactor branch.
+##
+## refactor/virtual-panel
+##   - setup_display(), clear_display()
+##   - setup_gpio(), configure_gpio(), configure_single_gpio(), clean_gpio()
+##   - handle_gpio_event()
+##   - initialize(), get_settings_defaults(), on_settings_save()
+##   - bcm2board()
+##   - start_display_timer(), stop_display_timer(), trigger_display_timeout()
+## refactor/modular-screens
+##   - handle_button_press()
+##   - next_mode(), try_cancel(), clear_cancel(), try_play(), try_pause()
+##   - update_ui() and all other update_ui_***()
+##   - _screen_mode, ScreenModes
+##   - on_event(), on_print_progress(), on_slicing_progress()
+##   - start_system_timer(), check_system_stats()
+##   - set_printer_state()
+
+## COMMON INTERFACE
+##   The following shall be stable from the top-level refactor branch.
+##
+## Display_panelPlugin.disp
+##   - disp.width, disp.height
+##   - disp.fill(n)
+##   - disp.show()
+##   - disp.poweroff(), disp.poweron()
+##   - disp.image(img)
+## Display_panelPlugin.handle_button_press(label)
+## Display_panelPlugin.update_ui()
+## Display_panelPlugin.start_system_timer()
+## Display_panelPlugin.start_display_timer(reconfigure)
+
 class ScreenModes(Enum):
 	PRINT = 1
 	PRINTER = 2
@@ -519,18 +554,30 @@ class Display_panelPlugin(octoprint.plugin.StartupPlugin,
 				else:
 					self.start_display_timer()
 				label = self.input_pinset[channel]
-				if label == 'cancel':
-					self.try_cancel()
+
+				self.handle_button_press(label)
+		except Exception as ex:
+			self.log_error(ex)
+			pass
+
+	def handle_button_press(self, label):
+		"""
+		Take action on a button press with the given name (such as 'cancel' or 'play')
+		"""
+
+		try:
+			if label == 'cancel':
+				self.try_cancel()
+			else:
+				if self._cancel_timer is not None:
+					self.clear_cancel()
 				else:
-					if self._cancel_timer is not None:
-						self.clear_cancel()
-					else:
-						if label == 'mode':
-							self.next_mode()
-						if label == 'play':
-							self.try_play()
-						if label == 'pause':
-							self.try_pause()
+					if label == 'mode':
+						self.next_mode()
+					if label == 'play':
+						self.try_play()
+					if label == 'pause':
+						self.try_pause()
 		except Exception as ex:
 			self.log_error(ex)
 			pass
